fc82d9d89558ba842b05217c1b063fbc26d764aa
Kiril Vatev <vatev.1@gmail.com>
2020-01-31 19:00:30 -0500

    removing is-heic, adding more complete heic detection

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const isHeic = require('is-heic');
 const libheif = require('libheif-js');
+
+const uint8ArrayUtf8ByteString = (array, start, end) => {
+  return String.fromCharCode(...array.slice(start, end));
+};
+
+// brands explained: https://github.com/strukturag/libheif/issues/83
+// code adapted from: https://github.com/sindresorhus/file-type/blob/6f901bd82b849a85ca4ddba9c9a4baacece63d31/core.js#L428-L438
+const isHeic = (buffer) => {
+  const brandMajor = uint8ArrayUtf8ByteString(buffer, 8, 12).replace('\0', ' ').trim();
+
+  switch (brandMajor) {
+    case 'mif1':
+      return true; // {ext: 'heic', mime: 'image/heif'};
+    case 'msf1':
+      return true; // {ext: 'heic', mime: 'image/heif-sequence'};
+    case 'heic':
+    case 'heix':
+      return true; // {ext: 'heic', mime: 'image/heic'};
+    case 'hevc':
+    case 'hevx':
+      return true; // {ext: 'heic', mime: 'image/heic-sequence'};
+  }
+
+  return false;
+};
 
 const decodeBuffer = async inputBuffer => {
   const decoder = new libheif.HeifDecoder();
